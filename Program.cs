0a99413b281683b3bf2459e31541c50de80069e4
Zoran Maravic <zokicpokic@gmail.com>
2018-05-30 11:04:14 +0200

    initial upload

---
+++ Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage.DataMovement;
+
+namespace DMLibSample
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Enter Storage account name:");           
+            string accountName = Console.ReadLine();
+
+            Console.WriteLine("\nEnter Storage account key:");           
+            string accountKey = Console.ReadLine();
+
+            string storageConnectionString = "DefaultEndpointsProtocol=https;AccountName=" + accountName + ";AccountKey=" + accountKey;
+            CloudStorageAccount account = CloudStorageAccount.Parse(storageConnectionString);
+
+            ExecuteChoice(account);
+        }
+
+        public static void ExecuteChoice(CloudStorageAccount account)
+        {
+            Console.WriteLine("\nWhat type of transfer would you like to execute?\n1. Local file --> Azure Blob\n2. Local directory --> Azure Blob directory\n3. URL (e.g. Amazon S3 file) --> Azure Blob\n4. Azure Blob --> Azure Blob");
+            int choice = int.Parse(Console.ReadLine());
+
+            if(choice == 1)
+            {
+                TransferLocalFileToAzureBlob(account).Wait();
+            }
+            else if(choice == 2)
+            {
+                TransferLocalDirectoryToAzureBlobDirectory(account).Wait();
+            }
+            else if(choice == 3)
+            {
+                TransferUrlToAzureBlob(account).Wait();
+            }
+            else if(choice == 4)
+            {
+                TransferAzureBlobToAzureBlob(account).Wait();
+            }
+        }
+
+        public static async Task TransferLocalFileToAzureBlob(CloudStorageAccount account)
+        { 
+            string localFilePath = GetSourcePath();
+            CloudBlockBlob blob = GetBlob(account);
+            Console.WriteLine("\nTransfer started...");
+            await TransferManager.UploadAsync(localFilePath, blob);
+            Console.WriteLine("\nTransfer operation complete.");
+            ExecuteChoice(account);
+        }
+
+        public static async Task TransferLocalDirectoryToAzureBlobDirectory(CloudStorageAccount account)
+        { 
+
+        }
+
+        public static async Task TransferUrlToAzureBlob(CloudStorageAccount account)
+        {
+
+        }
+
+        public static async Task TransferAzureBlobToAzureBlob(CloudStorageAccount account)
+        {
+
+        }
+
+        public static string GetSourcePath()
+        {
+            Console.WriteLine("\nProvide path for source:");
+            string sourcePath = Console.ReadLine();
+
+            return sourcePath;
+        }
+
+        public static CloudBlockBlob GetBlob(CloudStorageAccount account)
+        {
+            CloudBlobClient blobClient = account.CreateCloudBlobClient();
+
+            Console.WriteLine("\nProvide name of Blob container:");
+            string containerName = Console.ReadLine();
+            CloudBlobContainer container = blobClient.GetContainerReference(containerName);
+            container.CreateIfNotExistsAsync().Wait();
+
+            Console.WriteLine("\nProvide name of new Blob:");
+            string blobName = Console.ReadLine();
+            CloudBlockBlob blob = container.GetBlockBlobReference(blobName);
+
+            return blob;
+        }
+    }
+}
